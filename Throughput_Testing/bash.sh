d08a2ed2c7737aaee1932c36aa9850ec4a8f2f3b
ASHISH GUPTA <ashishggupta0201@gmail.com>
2020-04-27 17:54:26 +0530

    Update bash.sh

--- Throughput_Testing/bash.sh
+++ Throughput_Testing/bash.sh
@@ -21,51 +21,65 @@ stringsForLossAndCorruptAndReorder=(
     '70'
     '90'
 )
+out=`sudo tc qdisc add dev wlp3s0 root netem delay 10ms`
 for i in "${stringsForDelay[@]}"
 do
    echo ${i} >>Delay.txt
-   out=`sudo tc qdisc add dev wlp3s0 root netem delay ${i}ms`
+   out=`sudo tc qdisc change dev wlp3s0 root netem delay ${i}ms`
+   out=`python test1.py &`
+   c=${out}
    out=`python test.py`
    echo ${out} >> Delay.txt
-   out=`sudo tc qdisc del dev wlp3s0 root netem delay ${i}ms`
+   out=`kill ${c}`
    # or do whatever with individual element of the array
 done
 
 for i in "${stringsForDelay[@]}"
 do
+    out=`sudo tc qdisc change dev wlp3s0 root netem delay ${i}ms`
     for j in "${stringsForLossAndCorruptAndReorder[@]}"
     do 
 	echo ${i} >>DelayAndLoss.txt
 	echo ${j} >> DelayAndLoss.txt
-        out=`sudo tc qdisc add dev wlp3s0 root netem delay ${i}ms`
 	out=`sudo tc qdisc change dev wlp3s0 root netem loss ${j}%`
+        out=`python test1.py &`
+        c=${out}
         out=`python test.py`
-	echo ${out} >> DelayAndLoss.txt
-        out=`sudo tc qdisc del dev wlp3s0 root netem delay ${i}ms`
+        echo ${out} >> DelayAndLoss.txt
+        out=`kill ${c}`
     done
 done
 
+out=`sudo tc qdisc del dev wlp3s0 root netem delay 10ms`
+out=`sudo tc qdisc add dev wlp3s0 root netem delay 10ms`
 for i in "${stringsForDelay[@]}"
 do
+    out=`sudo tc qdisc change dev wlp3s0 root netem delay ${i}ms
     for j in "${stringsForLossAndCorruptAndReorder[@]}"
     do 
 	echo ${i} >>DelayAndCorrupt.txt
 	echo ${j} >> DelayAndCorrupt.txt
-        out=`sudo tc qdisc add dev wlp3s0 root netem delay ${i}ms`
-	out=`sudo tc qdisc change dev wlp3s0 root netem corrupt ${j}%`
+        out=`sudo tc qdisc change dev wlp3s0 root netem corrupt ${j}%`
+        out=`python test1.py &`
+        c= ${out}
         out=`python test.py`
-	echo ${out} >> DelayAndCorrupt.txt
-        out=`sudo tc qdisc del dev wlp3s0 root netem delay ${i}ms`
+        echo ${out} >> DelayAndCorrupt.txt
+        out=`kill ${c}`
     done
 done
+
+out=`sudo tc qdisc del dev wlp3s0 root netem delay 10ms`
+out=`sudo tc qdisc add dev wlp3s0 root netem delay 10ms`
 
 for i in "${stringsForLossAndCorruptAndReorder[@]}"
 do
    echo ${i} >>Reorder.txt
-   out=`sudo  tc qdisc add dev wlp3s0 root netem delay 10ms reorder ${i}%`
+   out=`sudo  tc qdisc change dev wlp3s0 root netem delay 10ms reorder ${i}%`
+   out=`python test1.py &`
+   c=${out}
    out=`python test.py`
    echo ${out} >> Reorder.txt
-   out=`sudo  tc qdisc del dev wlp3s0 root netem delay 10ms reorder ${i}%`
+   out=`kill ${c}`
    # or do whatever with individual element of the array
 done
-
+out=`sudo tc qdisc del dev wlp3s0 root netem delay 10ms`
